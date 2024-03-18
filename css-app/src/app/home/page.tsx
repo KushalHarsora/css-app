19727620829fcecb6d558a0d4a9ed8df5fc1368d
kushal <kushalharsora13@gmail.com>
2024-03-18 20:17:00 +0530

    Initial Commit For CSS-App

---
+++ css-app/src/app/home/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className=" flex flex-col justify-center items-center h-screen w-full">
+      <p className=" text-4xl">
+        Home Page
+      </p>
+    </div>
+  )
+}
---
+++ css-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className=" flex flex-col justify-center items-center h-screen w-full">
+      <p className=" text-4xl">
+        Login Page
+      </p>
+    </div>
+  );
+}
