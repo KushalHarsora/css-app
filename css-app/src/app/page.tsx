4b64279f0eb520918b9c8d325be5a65b48fcf332
Kushal Harsora <kushalharsora13@gmail.com>
2024-04-01 09:56:02 +0530

    Few Changes

--- css-app/src/app/page.tsx
+++ css-app/src/app/page.tsx
@@ -1,13 +1,42 @@
 'use client';
 
 import React from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
   return (
-    <div className=" flex flex-col justify-center items-center h-screen w-full">
-      <p className=" text-4xl">
-        Login Page
-      </p>
+    <div className=" flex flex-col justify-center items-center align-middle h-screen w-full gap-2 bg-slate-100">
+      <div className=" flex flex-col gap-6 p-6 justify-center items-center align-middle border border-black rounded-md bg-white">
+        <p className=" text-4xl">
+          Login Page
+        </p>
+        <div className=" flex justify-center items-center align-middle ">
+          <input
+            className=" w-4/5 h-10 border border-black rounded-md p-2"
+            type="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className=" flex justify-center items-center align-middle ">
+          <input
+            className=" w-4/5 h-10 border border-black rounded-md p-2"
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </div>
+        <div>
+          <button
+            className=" border border-black rounded-md p-3 cursor-pointer hover:text-white hover:bg-black"
+            onClick={() => router.push('/home')}
+            >
+            submit
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
